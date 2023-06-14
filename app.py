42bbdb8943889706a5508e45c67ae1d5bba1c508
Khizer Tariq <khizertariq633@gmail.com>
2023-06-14 12:12:04 +0500

    code cleaning

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import json
-import os
 import requests
 from flask import Flask, render_template, request, jsonify
 import collections 
@@ -7,10 +5,8 @@ import collections.abc
 from pptx import Presentation
 
 API_KEY = "KEY"
-
 app = Flask(__name__)
 slides = []
-
 @app.route("/")
 def index():
     title = request.args.get("title")
@@ -21,11 +17,7 @@ def index():
     prompt = f"""
     Give content for {num_slides} slides on the topic '{title}' for Grade {grade} Students 
     """
-    print()
-    print("------------")
-    print(prompt)  # Debugger Check
-    print()
-    print("------------")
+    print(prompt)
     response = requests.post(
         "https://api.openai.com/v1/engines/text-davinci-003/completions",
         headers={
@@ -39,14 +31,9 @@ def index():
         }
     )
     response_json = response.json()
-    #print(response_json)
     slides = response_json["choices"][0]["text"].split("\n")
-    # print(slides)
-    # this variable is for testing slides variable indexing
     slide_list = slides
-    # print(slide_list)
     slide_list_filtered = [slide for slide in slide_list if slide != '']
-
     presentation = Presentation()
     for i in range(0, len(slide_list_filtered), 2):
         slide_number = slide_list_filtered[i]
@@ -56,9 +43,7 @@ def index():
         title.text = slide_number
         content = slide.placeholders[1]
         content.text = slide_description
-
     presentation.save(r"C:\Users\dell\Desktop\Slide Generator\SlidesGenerator\templates\output.pptx")
-
     return render_template("index.html", slides=slides)
 
 if __name__ == "__main__":
